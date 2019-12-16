6c028ea0d9c36d6b48a0bcea9b5b354a829482bd
walkeringDo <873657376@qq.com>
2019-12-16 14:15:43 +0800

    c# 基础类库

---
+++ Utility基础类大全/Json/ConvertJson.cs
@@ -0,0 +1,500 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Reflection;
+using System.Collections;
+using System.Data.Common;
+using System.Web.Script.Serialization;
+
+namespace Utilities
+{
+    //JSON转换类
+    public class ConvertJson
+    {
+        #region 私有方法
+        /// <summary>
+        /// 过滤特殊字符
+        /// </summary>
+        private static string String2Json(String s)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s.ToCharArray()[i];
+                switch (c)
+                {
+                    case '\"':
+                        sb.Append("\\\""); break;
+                    case '\\':
+                        sb.Append("\\\\"); break;
+                    case '/':
+                        sb.Append("\\/"); break;
+                    case '\b':
+                        sb.Append("\\b"); break;
+                    case '\f':
+                        sb.Append("\\f"); break;
+                    case '\n':
+                        sb.Append("\\n"); break;
+                    case '\r':
+                        sb.Append("\\r"); break;
+                    case '\t':
+                        sb.Append("\\t"); break;
+                    default:
+                        sb.Append(c); break;
+                }
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 格式化字符型、日期型、布尔型
+        /// </summary>
+        private static string StringFormat(string str, Type type)
+        {
+            if (type == typeof(string))
+            {
+                str = String2Json(str);
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(DateTime))
+            {
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(bool))
+            {
+                str = str.ToLower();
+            }
+            else if (type != typeof(string) && string.IsNullOrEmpty(str))
+            {
+                str = "\"" + str + "\"";
+            }
+            return str;
+        }
+        #endregion
+
+        #region List转换成Json
+        /// <summary>
+        /// List转换成Json
+        /// </summary>
+        public static string ListToJson<T>(IList<T> list)
+        {
+            object obj = list[0];
+            return ListToJson<T>(list, obj.GetType().Name);
+        }
+
+        /// <summary>
+        /// List转换成Json 
+        /// </summary>
+        public static string ListToJson<T>(IList<T> list, string jsonName)
+        {
+            StringBuilder Json = new StringBuilder();
+            if (string.IsNullOrEmpty(jsonName)) jsonName = list[0].GetType().Name;
+            Json.Append("{\"" + jsonName + "\":[");
+            if (list.Count > 0)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    T obj = Activator.CreateInstance<T>();
+                    PropertyInfo[] pi = obj.GetType().GetProperties();
+                    Json.Append("{");
+                    for (int j = 0; j < pi.Length; j++)
+                    {
+                        Type type = pi[j].GetValue(list[i], null).GetType();
+                        Json.Append("\"" + pi[j].Name.ToString() + "\":" + StringFormat(pi[j].GetValue(list[i], null).ToString(), type));
+
+                        if (j < pi.Length - 1)
+                        {
+                            Json.Append(",");
+                        }
+                    }
+                    Json.Append("}");
+                    if (i < list.Count - 1)
+                    {
+                        Json.Append(",");
+                    }
+                }
+            }
+            Json.Append("]}");
+            return Json.ToString();
+        }
+        #endregion
+
+        #region 对象转换为Json
+        /// <summary> 
+        /// 对象转换为Json 
+        /// </summary> 
+        /// <param name="jsonObject">对象</param> 
+        /// <returns>Json字符串</returns> 
+        public static string ToJson(object jsonObject)
+        {
+            string jsonString = "{";
+            PropertyInfo[] propertyInfo = jsonObject.GetType().GetProperties();
+            for (int i = 0; i < propertyInfo.Length; i++)
+            {
+                object objectValue = propertyInfo[i].GetGetMethod().Invoke(jsonObject, null);
+                string value = string.Empty;
+                if (objectValue is DateTime || objectValue is Guid || objectValue is TimeSpan)
+                {
+                    value = "'" + objectValue.ToString() + "'";
+                }
+                else if (objectValue is string)
+                {
+                    value = "'" + ToJson(objectValue.ToString()) + "'";
+                }
+                else if (objectValue is IEnumerable)
+                {
+                    value = ToJson((IEnumerable)objectValue);
+                }
+                else
+                {
+                    value = ToJson(objectValue.ToString());
+                }
+                jsonString += "\"" + ToJson(propertyInfo[i].Name) + "\":" + value + ",";
+            }
+            jsonString.Remove(jsonString.Length - 1, jsonString.Length);
+            return jsonString + "}";
+        }
+        #endregion
+
+        #region 对象集合转换Json
+        /// <summary> 
+        /// 对象集合转换Json 
+        /// </summary> 
+        /// <param name="array">集合对象</param> 
+        /// <returns>Json字符串</returns> 
+        public static string ToJson(IEnumerable array)
+        {
+            string jsonString = "[";
+            foreach (object item in array)
+            {
+                jsonString += ToJson(item) + ",";
+            }
+            jsonString.Remove(jsonString.Length - 1, jsonString.Length);
+            return jsonString + "]";
+        }
+        #endregion
+
+        #region 普通集合转换Json
+        /// <summary> 
+        /// 普通集合转换Json 
+        /// </summary> 
+        /// <param name="array">集合对象</param> 
+        /// <returns>Json字符串</returns> 
+        public static string ToArrayString(IEnumerable array)
+        {
+            string jsonString = "[";
+            foreach (object item in array)
+            {
+                jsonString = ToJson(item.ToString()) + ",";
+            }
+            jsonString.Remove(jsonString.Length - 1, jsonString.Length);
+            return jsonString + "]";
+        }
+        #endregion
+
+        #region  DataSet转换为Json
+        /// <summary> 
+        /// DataSet转换为Json 
+        /// </summary> 
+        /// <param name="dataSet">DataSet对象</param> 
+        /// <returns>Json字符串</returns> 
+        public static string ToJson(DataSet dataSet)
+        {
+            string jsonString = "{";
+            foreach (DataTable table in dataSet.Tables)
+            {
+                jsonString += "\"" + table.TableName + "\":" + ToJson(table) + ",";
+            }
+            jsonString = jsonString.TrimEnd(',');
+            return jsonString + "}";
+        }
+        #endregion
+
+        #region Datatable转换为Json
+        /// <summary> 
+        /// Datatable转换为Json 
+        /// </summary> 
+        /// <param name="table">Datatable对象</param> 
+        /// <returns>Json字符串</returns> 
+        public static string ToJson(DataTable dt)
+        {
+            StringBuilder jsonString = new StringBuilder();
+            jsonString.Append("[");
+            DataRowCollection drc = dt.Rows;
+            for (int i = 0; i < drc.Count; i++)
+            {
+                jsonString.Append("{");
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    string strKey = dt.Columns[j].ColumnName;
+                    string strValue = drc[i][j].ToString();
+                    Type type = dt.Columns[j].DataType;
+                    jsonString.Append("\"" + strKey + "\":");
+                    strValue = StringFormat(strValue, type);
+                    if (j < dt.Columns.Count - 1)
+                    {
+                        jsonString.Append(strValue + ",");
+                    }
+                    else
+                    {
+                        jsonString.Append(strValue);
+                    }
+                }
+                jsonString.Append("},");
+            }
+            jsonString.Remove(jsonString.Length - 1, 1);
+            jsonString.Append("]");
+            return jsonString.ToString();
+        }
+
+        /// <summary>
+        /// DataTable转换为Json 
+        /// </summary>
+        public static string ToJson(DataTable dt, string jsonName)
+        {
+            StringBuilder Json = new StringBuilder();
+            if (string.IsNullOrEmpty(jsonName)) jsonName = dt.TableName;
+            Json.Append("{\"" + jsonName + "\":[");
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    Json.Append("{");
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        Type type = dt.Rows[i][j].GetType();
+                        Json.Append("\"" + dt.Columns[j].ColumnName.ToString() + "\":" + StringFormat(dt.Rows[i][j].ToString(), type));
+                        if (j < dt.Columns.Count - 1)
+                        {
+                            Json.Append(",");
+                        }
+                    }
+                    Json.Append("}");
+                    if (i < dt.Rows.Count - 1)
+                    {
+                        Json.Append(",");
+                    }
+                }
+            }
+            Json.Append("]}");
+            return Json.ToString();
+        }
+        #endregion
+
+        #region DataReader转换为Json
+        /// <summary> 
+        /// DataReader转换为Json 
+        /// </summary> 
+        /// <param name="dataReader">DataReader对象</param> 
+        /// <returns>Json字符串</returns> 
+        public static string ToJson(DbDataReader dataReader)
+        {
+            StringBuilder jsonString = new StringBuilder();
+            jsonString.Append("[");
+            while (dataReader.Read())
+            {
+                jsonString.Append("{");
+                for (int i = 0; i < dataReader.FieldCount; i++)
+                {
+                    Type type = dataReader.GetFieldType(i);
+                    string strKey = dataReader.GetName(i);
+                    string strValue = dataReader[i].ToString();
+                    jsonString.Append("\"" + strKey + "\":");
+                    strValue = StringFormat(strValue, type);
+                    if (i < dataReader.FieldCount - 1)
+                    {
+                        jsonString.Append(strValue + ",");
+                    }
+                    else
+                    {
+                        jsonString.Append(strValue);
+                    }
+                }
+                jsonString.Append("},");
+            }
+            dataReader.Close();
+            jsonString.Remove(jsonString.Length - 1, 1);
+            jsonString.Append("]");
+            return jsonString.ToString();
+        }
+        #endregion
+
+        /// <summary>
+        /// 将object对象转换为实体对象
+        /// </summary>
+        /// <typeparam name="T">实体对象类名</typeparam>
+        /// <param name="asObject">object对象</param>
+        /// <returns></returns>
+        public static T ConvertObject<T>(object asObject) where T : new()
+        {
+            var serializer = new JavaScriptSerializer();
+            //将object对象转换为json字符
+            var json = serializer.Serialize(asObject);
+            //将json字符转换为实体对象
+            var t = serializer.Deserialize<T>(json);
+            return t;
+        }
+
+
+        /// <summary>
+        /// 把对象序列化 JSON 字符串 
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="obj">对象实体</param>
+        /// <returns>JSON字符串</returns>
+        public static string GetJson<T>(T obj)
+        {
+            //记住 添加引用 System.ServiceModel.Web 
+            /**
+             * 如果不添加上面的引用,System.Runtime.Serialization.Json; Json是出不来的哦
+             * */
+            DataContractJsonSerializer json = new DataContractJsonSerializer(typeof(T));
+            using (MemoryStream ms = new MemoryStream())
+            {
+                json.WriteObject(ms, obj);
+                string szJson = Encoding.UTF8.GetString(ms.ToArray());
+                return szJson;
+            }
+        }
+
+
+        /// <summary>
+        /// JSON字符串转实体类
+        /// </summary>
+        /// <param name="json"></param>
+        public static CardValue JsonMy(string json)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();   //实例化一个能够序列化数据的类
+            CardValue list = js.Deserialize<CardValue>(json);    //将json数据转化为对象类型并赋值给list
+
+            return list;
+        }
+
+        /// <summary>
+        /// JSON转list 实体类
+        /// </summary>
+        /// <param name="json"></param>
+        public static JZdata JsonMy(string json)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();   //实例化一个能够序列化数据的类
+            JZdata list = js.Deserialize<JZdata>(json);    //将json数据转化为对象类型并赋值给list
+            string msg = list.msg;
+            string BuyCounter = list.BuyCounter;
+            string CardState = list.CardState;
+            string CardType = list.CardType;
+            string CheckSumCode = list.CheckSumCode;
+            string CustomerCode = list.CustomerCode;
+            string MeterID = list.MeterID;
+            string NotifyByPurchased = list.NotifyByPurchased;
+            string OverdraftThreshold = list.OverdraftThreshold;
+            string Price_1 = list.Price_1;
+            string Price_2 = list.Price_2;
+            string Price_3 = list.Price_3;
+            string Purchased = list.Purchased;
+            string RndCode = list.RndCode;
+            string Step_1 = list.Step_1;
+            string Step_2 = list.Step_2;
+            string Total = list.Total;
+            return list;
+        }
+
+        /// <summary>
+        /// 卡返回类
+        /// </summary>
+        public class CardResult
+        {
+            /// <summary>
+            /// 状态码(操作是否成功)
+            /// </summary>
+            public bool Status { get; set; }
+
+            /// <summary>
+            /// 返回消息
+            /// </summary>
+            public string Message { get; set; }
+
+            /// <summary>
+            /// 返回数据
+            /// </summary>
+            public object Data { get; set; }
+        }
+
+        /// <summary>
+        /// 精致卡表数据类型
+        /// </summary>
+        public class JZdata
+        {
+            /// <summary>
+            /// 报警提示
+            /// </summary>
+            public string msg { get; set; }
+            /// <summary>
+            /// 购买次数
+            /// </summary>
+            public string BuyCounter { get; set; }
+            /// <summary>
+            /// 表状态
+            /// </summary>
+            public string CardState { get; set; }
+            /// <summary>
+            /// 表类型
+            /// </summary>
+            public string CardType { get; set; }
+            /// <summary>
+            /// 校验码
+            /// </summary>
+            public string CheckSumCode { get; set; }
+            /// <summary>
+            /// 客户代码
+            /// </summary>
+            public string CustomerCode { get; set; }
+            /// <summary>
+            /// 仪表号
+            /// </summary>
+            public string MeterID { get; set; }
+            /// <summary>
+            /// 通知购买
+            /// </summary>
+            public string NotifyByPurchased { get; set; }
+            /// <summary>
+            /// 透支阈值
+            /// </summary>
+            public string OverdraftThreshold { get; set; }
+            /// <summary>
+            /// 价格1
+            /// </summary>
+            public string Price_1 { get; set; }
+            /// <summary>
+            /// 价格2
+            /// </summary>
+            public string Price_2 { get; set; }
+            /// <summary>
+            /// 价格3
+            /// </summary>
+            public string Price_3 { get; set; }
+            /// <summary>
+            /// 购买
+            /// </summary>
+            public string Purchased { get; set; }
+            /// <summary>
+            /// RND码
+            /// </summary>
+            public string RndCode { get; set; }
+            /// <summary>
+            /// 一阶
+            /// </summary>
+            public string Step_1 { get; set; }
+            /// <summary>
+            /// 二阶
+            /// </summary>
+            public string Step_2 { get; set; }
+            /// <summary>
+            /// 总量
+            /// </summary>
+            public string Total { get; set; }
+
+        }
+
+    }
+}
